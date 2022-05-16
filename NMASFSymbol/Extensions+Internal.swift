cecbda3177a9be354583d5226da3569e075a892b
Monica Rondón <monirondon15@gmail.com>
2022-05-16 17:22:57 +0200

    [setup] Adds current receipts NMASFSymbol

---
+++ NMASFSymbol/Extensions+Internal.swift
@@ -0,0 +1,23 @@
+//
+//  Image+Extensions.swift
+//  NMASFSymbol
+//
+//  Created by Monica Rondón on 2/9/22.
+//
+
+import Foundation
+
+
+internal extension RawRepresentable {
+    
+//    var rawString: String { String(describing: self) }
+    
+    var caseString: String { String(describing: self.self) }
+
+}
+
+
+//internal extension RawRepresentable where RawValue == String {
+//    var rawString: String { return rawValue }
+//}
+
---
+++ NMASFSymbol/Extensions+Public.swift
@@ -0,0 +1,108 @@
+//
+//  Extensions+Public.swift
+//  NMASFSymbol
+//
+//  Created by Monica Rondón on 2/10/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+// MARK: - UIImage+Extensions
+public extension UIImage {
+    
+    /// Creates an image object that contains a system symbol image.
+    ///
+    /// - parameter symbol: The `SFSymbol` for the system symbol image.
+    convenience init?(symbol: SFSymbol) {
+        self.init(systemName: symbol.systemName)
+    }
+
+    
+    /// Creates an image object that contains a system symbol image with the
+    /// specified configuration.
+    ///
+    /// - parameters:
+    ///   - symbol: The `SFSymbol` for the system symbol image.
+    ///   - configuration: The image configuration that you want.
+    ///   Use this parameter to specify traits and other details that define
+    ///   which variant of the image you want. For example, you can request a
+    ///   symbol image with a specified weight.
+    convenience init?(symbol: SFSymbol, withConfiguration configuration: UIImage.SymbolConfiguration) {
+        self.init(systemName: symbol.systemName, withConfiguration: configuration)
+    }
+    
+    
+    /// Creates an image object that contains a system symbol image appropriate
+    /// for the specified traits.
+    ///
+    /// - parameters:
+    ///   - symbol: The `SFSymbol` for the system symbol image.
+    ///   - traitCollection: The traits associated with the intended environment
+    ///   for the image.
+    ///   Use this parameter to ensure that the correct variant of the image
+    ///   is loaded. If you specify nil, this method uses the traits associated
+    ///   with the main screen.
+    convenience init?(symbol: SFSymbol, compatibleWith traitCollection: UITraitCollection?) {
+        self.init(systemName: symbol.systemName, compatibleWith: traitCollection)
+    }
+        
+}
+
+
+
+// MARK: - Image+Extensions
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public extension Image {
+    
+    /// Creates a system symbol image.
+    ///
+    /// - Parameter symbol: The `SFSymbol` for the system symbol image.
+    @available(macOS 11.0, *)
+    init(symbol: SFSymbol) {
+        self.init(systemName: symbol.systemName)
+    }
+    
+}
+
+// MARK: - Label+Extensions
+@available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
+extension Label where Title == Text, Icon == Image {
+    
+    /// Creates a label with a system icon image and a title generated from a
+    /// localized string.
+    ///
+    /// - Parameters:
+    ///    - titleKey: A title generated from a localized string.
+    ///    - symbol: The symbol of the image resource.
+    public init(_ titleKey: LocalizedStringKey, symbol: SFSymbol) {
+        self.init(titleKey, systemImage: symbol.systemName)
+    }
+        
+    /// Creates a label with a system icon image and a title generated from a
+    /// string.
+    ///
+    /// - Parameters:
+    ///    - title: A string used as the label's title.
+    ///    - symbol: The symbol of the image resource.
+    public init<S>(_ title: S, symbol: SFSymbol) where S : StringProtocol {
+        self.init(title, systemImage: symbol.systemName)
+    }
+    
+    /// Creates a label with a system icon image and a the `SFSymbol.displayname` as a title
+    /// - Parameter symbol: The symbol of the image resource.
+    public init(labeledSymbol symbol: SFSymbol) {
+        self.init(symbol.displayName, systemImage: symbol.systemName)
+    }
+    
+    /// Creates a label with a system icon image with an empty `String` as a title
+    /// string.
+    ///
+    /// - Parameters:
+    ///    - symbol: The symbol of the image resource.
+    public init(symbol: SFSymbol) {
+        self.init(String(), systemImage: symbol.systemName)
+    }
+
+}
