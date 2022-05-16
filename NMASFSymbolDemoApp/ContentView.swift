e07c6b342cf98d6d0f1afeaa251789e24abaa96e
Monica Rondón <monirondon15@gmail.com>
2022-05-16 17:45:16 +0200

    [demo] Adds demo app, uses NMA in product name again

---
+++ NMASFSymbolDemoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NMASFSymbolDemoApp
+//
+//  Created by Monica Rondón on 5/16/22.
+//
+
+import SwiftUI
+import NMASFSymbol
+
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NMASFSymbolDemoApp/NMASFSymbolDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NMASFSymbolDemoAppApp.swift
+//  NMASFSymbolDemoApp
+//
+//  Created by Monica Rondón on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct NMASFSymbolDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
