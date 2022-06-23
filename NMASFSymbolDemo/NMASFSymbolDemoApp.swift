e31e45e56603caa39274af323842565fff6a0abd
Monica Rondón <monirondon15@gmail.com>
2022-06-23 19:22:49 +0200

    Adds new demo and duplicates assets

---
+++ NMASFSymbolDemo/NMASFSymbolDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NMASFSymbolDemoApp.swift
+//  NMASFSymbolDemo
+//
+//  Created by Monica Rondón on 6/23/22.
+//
+
+import SwiftUI
+
+@main
+struct NMASFSymbolDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
